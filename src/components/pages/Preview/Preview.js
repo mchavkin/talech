08ef5720acfc6e554da495165bbc4a1c57371819
mchavkin <mchavkin@gmail.com>
2020-09-02 01:26:28 +0300

    Simple preview

--- src/components/pages/Preview/Preview.js
+++ src/components/pages/Preview/Preview.js
@@ -1,11 +1,13 @@
 import React from "react";
 import {useParams} from "react-router";
+import {useSelector} from "react-redux";
 
-export default function Preview(){
-    const {id} = useParams()
-    return(
+export default function Preview() {
+    const {id} = useParams();
+    const entry = useSelector(state => state.entries.find(entry => id === entry.ean));
+    return (
         <div>
-            {`Product # ${id}`}
+            {JSON.stringify(entry)}
         </div>
     )
 }
