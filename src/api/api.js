d8c0aaa3bff3dad811968e76d47300c911fc4c99
mchavkin <mchavkin@gmail.com>
2020-09-01 13:42:41 +0300

    timeout added to api

--- src/api/api.js
+++ src/api/api.js
@@ -1,14 +1,18 @@
-//to emulate Rest API all persistence functions just wrapped in Promises
+//to emulate Rest API all persistence functions
+// wrapped in Promises with 1 s timeout
 
 import * as persistence from '../persistence/storage'
 
-const wrapFunctionInPromise = (func) => async (...args) => {
-    try {
-        return await func(...args);
-    } catch (error) {
-        throw error;
-    }
-}
+const wrapFunctionInPromise = (func) => (...args) => new Promise((resolve, reject) => {
+    setTimeout(() => {
+        try{
+            resolve(func(...args));
+        }catch (error){
+            reject(error);
+        }
+    }, 1000);
+});
+
 
 const api = Object.getOwnPropertyNames(persistence)
     .map(funcName => (
